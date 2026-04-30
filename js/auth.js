bbf428652310c4d8e712c8f564f71b702dc23cac
Thiago Oliveira <thiagolboliveira@hotmail.com>
2026-04-30 01:07:03 -0300

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -10,5 +10,20 @@ console.log("AUTH + FIREBASE OK");
 
 // TESTE
 window.login = () => alert("login ok");
-window.register = () => alert("register ok");
+import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+
+const emailEl = () => document.getElementById("email");
+const senhaEl = () => document.getElementById("senha");
+
+window.register = async () => {
+  let email = emailEl().value;
+  let senha = senhaEl().value;
+
+  try {
+    await createUserWithEmailAndPassword(auth, email, senha);
+    alert("Conta criada!");
+  } catch (e) {
+    alert(e.message);
+  }
+};
 window.googleLogin = () => alert("google ok");
