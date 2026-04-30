76ead3ce5270171c7a3569f06180c8c1cb462256
Thiago Oliveira <thiagolboliveira@hotmail.com>
2026-04-30 02:24:50 -0300

    3
    3

--- js/auth.js
+++ js/auth.js
@@ -1,17 +1,3 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyB_UmbwA2O-7_rTLY_TmgRiUl2dP5tmmfM",
-  authDomain: "calcpro-b2f4c.firebaseapp.com",
-  projectId: "calcpro-b2f4c",
-  storageBucket: "calcpro-b2f4c.firebasestorage.app",
-  messagingSenderId: "999996315749",
-  appId: "1:999996315749:web:5667398eec16c0eb940e8c",
-  measurementId: "G-0BW51LT9G2"
-};
-
-export const app = initializeApp(firebaseConfig);
-
 import { app } from "./firebase.js";
 
 import {
@@ -24,36 +10,44 @@ import {
 
 const auth = getAuth(app);
 
-// inputs
 const emailEl = () => document.getElementById("email");
 const senhaEl = () => document.getElementById("senha");
 
 // LOGIN
-async function login() {
-  const email = emailEl().value;
-  const senha = senhaEl().value;
-
-  await signInWithEmailAndPassword(auth, email, senha);
-  window.location.href = "dashboard.html";
-}
+window.login = async () => {
+  try {
+    await signInWithEmailAndPassword(
+      auth,
+      emailEl().value,
+      senhaEl().value
+    );
+    window.location.href = "dashboard.html";
+  } catch (e) {
+    alert(e.message);
+  }
+};
 
 // CADASTRO
-async function register() {
-  const email = emailEl().value;
-  const senha = senhaEl().value;
-
-  await createUserWithEmailAndPassword(auth, email, senha);
-  alert("Conta criada!");
-}
+window.register = async () => {
+  try {
+    await createUserWithEmailAndPassword(
+      auth,
+      emailEl().value,
+      senhaEl().value
+    );
+    alert("Conta criada!");
+  } catch (e) {
+    alert(e.message);
+  }
+};
 
 // GOOGLE
-async function googleLogin() {
-  const provider = new GoogleAuthProvider();
-  await signInWithPopup(auth, provider);
-  window.location.href = "dashboard.html";
-}
-
-// expor funções pro HTML
-window.login = login;
-window.register = register;
-window.googleLogin = googleLogin;
+window.googleLogin = async () => {
+  try {
+    const provider = new GoogleAuthProvider();
+    await signInWithPopup(auth, provider);
+    window.location.href = "dashboard.html";
+  } catch (e) {
+    alert(e.message);
+  }
+};
