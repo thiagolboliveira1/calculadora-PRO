030f33ac54513d489396adcb3884d3366ef84c06
Thiago Oliveira <thiagolboliveira@hotmail.com>
2026-04-30 00:29:46 -0300

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,53 +1,23 @@
-// LOGIN
-window.login = async () => {
-  let email = emailEl().value;
-  let senha = senhaEl().value;
+import { signInWithRedirect } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
-  try {
-    await signInWithEmailAndPassword(auth, email, senha);
-    window.location.href = "dashboard.html";
-  } catch (error) {
-    alert(traduzErro(error.code));
-  }
-};
-
-// REGISTRO
-window.register = async () => {
-  let email = emailEl().value;
-  let senha = senhaEl().value;
-
-  try {
-    await createUserWithEmailAndPassword(auth, email, senha);
-    alert("Conta criada com sucesso!");
-  } catch (error) {
-    alert(traduzErro(error.code));
-  }
-};
-
-// GOOGLE
 window.googleLogin = async () => {
-  try {
     const provider = new GoogleAuthProvider();
-    await signInWithPopup(auth, provider);
-    window.location.href = "dashboard.html";
-  } catch (error) {
-    alert("Erro no login com Google");
-  }
+    await signInWithRedirect(auth, provider);
 };
 
-function traduzErro(code) {
-  switch (code) {
-    case "auth/email-already-in-use":
-      return "Esse email já está em uso.";
-    case "auth/invalid-email":
-      return "Email inválido.";
-    case "auth/weak-password":
-      return "A senha precisa ter pelo menos 6 caracteres.";
-    case "auth/user-not-found":
-      return "Usuário não encontrado.";
-    case "auth/wrong-password":
-      return "Senha incorreta.";
-    default:
-      return "Erro: " + code;
-  }
-}
+import { getRedirectResult } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+
+getRedirectResult(auth)
+  .then((result) => {
+    if (result) {
+      window.location.href = "dashboard.html";
+    }
+  })
+  .catch((error) => {
+    console.log(error);
+    alert("Erro no login com Google");
+  });
+
+  window.login = login;
+window.register = register;
+window.googleLogin = googleLogin;
