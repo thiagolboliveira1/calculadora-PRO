6d8a6a84798783492063ab12739bbc30be4e8fb3
Thiago Oliveira <thiagolboliveira@hotmail.com>
2026-04-30 01:02:43 -0300

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,4 +1,14 @@
-console.log("AUTH OK");
-window.register = () => alert("register ok");
+import { app } from "./firebase.js";
+
+import {
+  getAuth
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+
+const auth = getAuth(app);
+
+console.log("AUTH + FIREBASE OK");
+
+// TESTE
 window.login = () => alert("login ok");
+window.register = () => alert("register ok");
 window.googleLogin = () => alert("google ok");
